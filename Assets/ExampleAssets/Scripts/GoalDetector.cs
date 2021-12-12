1f53dc2553da28ab11e38618d415b2ffe6fe60e0
LAPTOP-K0TM5ROV\hoshi <hoshi527-jun.k.w.s.k@ezweb.ne.jp>
2021-12-12 18:53:31 +0900

    ゲームシステム部分の構築

---
+++ Assets/ExampleAssets/Scripts/GoalDetector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetector : MonoBehaviour
+{
+    [SerializeField] int num;
+    [SerializeField] GameObject GoalBall;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == GoalBall)
+        GetComponentInParent<GoalManager>().SetGoalNum(num);
+    }
+}
---
+++ Assets/ExampleAssets/Scripts/GoalManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalManager : MonoBehaviour
+{
+    [SerializeField] Text ResultText;
+    private bool[] goallist = new bool[2];
+    private void Start()
+    {
+        ResultText.enabled = false;
+    }
+
+    public void SetGoalNum(int i)
+    {
+        goallist[i] = true;
+        DetectGoal();
+    }
+
+    private void DetectGoal()
+    {
+        int goalnum = 0;
+        foreach(bool goal in goallist)
+        {
+            if (goal) goalnum += 1;
+        }
+        if(goalnum >= 2)
+        {
+            ResultText.text = "Goal";
+            ResultText.enabled = true;
+        }
+    }
+}
