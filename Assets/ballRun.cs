d910da024a68b2b74b5081117793900eb8eda7cb
iwanoridake <riwano.ikeda@fj.ics.keio.ac.jp>
2021-12-12 19:18:58 +0900

    y軸回転しないよう調整

--- Assets/ballRun.cs
+++ Assets/ballRun.cs
@@ -14,13 +14,24 @@ public class ballRun : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var dir = Vector3.zero;
+		float maxLimit = 3, minLimit = 360 - maxLimit;
 		Quaternion rotRH = Input.gyro.attitude ;
 		Vector3 vecrot = Input.gyro.rotationRateUnbiased;
 		
  		// 現在の自信の回転の情報を取得する。
-        Quaternion rot = new Quaternion(-rotRH.x, -rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(0f, -180f, 0f);;
+        Quaternion rot = new Quaternion(-rotRH.x, rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(0f, 180f, 0f);;
         // 合成して、自身に設定
         this.transform.rotation = rot;
+		
+		
+
+        //X軸回転
+        var localAngle = transform.localEulerAngles;
+        if (localAngle.y > maxLimit && localAngle.y < 180)
+            localAngle.y = maxLimit;
+        if (localAngle.y < minLimit && localAngle.y > 180)
+            localAngle.y = minLimit;
+        transform.localEulerAngles = localAngle;
  
 	}
 }
