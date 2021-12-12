b78d5ed1a2a2ba20f4f0526813af288c62df66db
iwanoridake <riwano.ikeda@fj.ics.keio.ac.jp>
2021-12-13 01:23:33 +0900

    スタート画面

---
+++ Assets/Scripts/LoadGameScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("afda");
+        SceneManager.LoadScene("GameScene");
+    }
+}
