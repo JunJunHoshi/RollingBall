d70483cc20fa0360f8551f386238b6036fda620e
iwanoridake <riwano.ikeda@fj.ics.keio.ac.jp>
2021-12-12 18:48:10 +0900

    傾き検知

---
+++ Assets/ballRun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class ballRun : MonoBehaviour {
+ 
+	private float  speed = 10.0f;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dir = Vector3.zero;
+		Quaternion rotRH = Input.gyro.attitude ;
+		Vector3 vecrot = Input.gyro.rotationRateUnbiased;
+		
+ 		// 現在の自信の回転の情報を取得する。
+        Quaternion rot = new Quaternion(-rotRH.x, -rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(0f, -180f, 0f);;
+        // 合成して、自身に設定
+        this.transform.rotation = rot;
+ 
+	}
+}
